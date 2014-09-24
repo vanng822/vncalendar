a13858358f59c9dbdaf06895dc72e5df7b11b0f8
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-09-24 09:03:08 +0200

    Fix fail messages

--- converter_test.go
+++ converter_test.go
@@ -10,10 +10,10 @@ func TestSolar2Lunar(t *testing.T) {
 		t.Errorf("Day expected to be 30 but got '%s'", result.Day)
 	}
 	if result.Month != 8 {
-		t.Errorf("Month expected to be 30 but got '%s'", result.Month)
+		t.Errorf("Month expected to be 8 but got '%s'", result.Month)
 	}
 	if result.Year != 2014 {
-		t.Errorf("Year expected to be 30 but got '%s'", result.Year)
+		t.Errorf("Year expected to be 2014 but got '%s'", result.Year)
 	}
 }
 
@@ -21,12 +21,12 @@ func TestSolar2Lunar(t *testing.T) {
 func TestLunar2solar(t *testing.T) {
 	result := Lunar2solar(2014, 8, 30, 0, 0)
 	if result.Day != 23 {
-		t.Errorf("Day expected to be 30 but got '%s'", result.Day)
+		t.Errorf("Day expected to be 23 but got '%s'", result.Day)
 	}
 	if result.Month != 9 {
-		t.Errorf("Month expected to be 30 but got '%s'", result.Month)
+		t.Errorf("Month expected to be 9 but got '%s'", result.Month)
 	}
 	if result.Year != 2014 {
-		t.Errorf("Year expected to be 30 but got '%s'", result.Year)
+		t.Errorf("Year expected to be 2014 but got '%s'", result.Year)
 	}
 }
