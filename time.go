fcf1c1620634403669407dca3976a797b7e1b386
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2016-11-15 04:13:34 +0100

    Lock timezone to 7

--- time.go
+++ time.go
@@ -6,14 +6,8 @@ import (
 )
 
 var (
-	TimeZoneOffset int
+	TimeZoneOffset = 7
 )
-
-func init() {
-	now := time.Now()
-	_, offset := now.Zone()
-	TimeZoneOffset = int(offset / 3600)
-}
 
 type VNDate struct {
 	solarTime      time.Time
